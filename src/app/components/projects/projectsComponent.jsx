31dff8da684ace91906c53d43c7c202d9fa31f52
ronaldo <ronaldo@beautitag.com>
2021-01-16 11:09:08 +0800

    project updated

--- src/app/components/projects/projectsComponent.jsx
+++ src/app/components/projects/projectsComponent.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Card, Button, Row, Col, Container } from 'react-bootstrap';
 import './projectsStyle.css';
+import kahonhkImage from '../../images/projects-images/kahonHK.PNG'
+import simpleTaskMgmt from '../../images/projects-images/simpleTaskMgmt.PNG'
 
 function ProjectsComponent() {
     const projects = [
         {
-            img: '',
+            img: kahonhkImage,
             title: 'Kahon.hk',
-            description: 'ReactJS, Redux, Bootstrap, Fontawesome, Mongodb, Express',
+            description: 'HTML, CSS, Node JS, Koa Framework, EJS',
             link: 'www.kahon.hk'
         },
         {
-            img: '',
+            img: simpleTaskMgmt,
             title: 'Simple Task Management',
             description: 'ReactJS, Redux, Bootstrap, Fontawesome, Mongodb, Express',
-            link: ''
+            link: 'https://simple-tak-management.netlify.app/'
         },
         {
             img: '',
@@ -41,7 +43,7 @@ function ProjectsComponent() {
                         <Card.Body>
                             <Card.Title>{project.title}</Card.Title>
                             <Card.Text>{project.description}</Card.Text>
-                            <Button variant="primary" onClick={() => window.location.replace(project.link)}>Check it out</Button>
+                            <Button variant="primary" onClick={() => window.location.replace(project.link)}>Check it out</Button>                            
                         </Card.Body>
                         </Card>
                     </Col>
